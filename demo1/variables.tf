aa67dc48864c839f0e4a13c133ba764bbee50af2
skodange <89379691+skodange@users.noreply.github.com>
2021-08-24 09:47:15 +0530

    Update variables.tf

--- demo1/variables.tf
+++ demo1/variables.tf
@@ -1,5 +1,11 @@
-variable "Name" {
+variable "s3_name" {
  description = "Name of S3 Bucket"
  type = string
- default = "Shreehari K"
+ default = "Shreehari-S3-Bucket"
+}
+
+variable "env_name" {
+ description = "Name of Environment"
+ type = string
+ default = "Demo"
 }
